274dd1a04f4f49f72568d14a9060a25f463961a2
Sohan K R <sonyd4d@live.com>
2021-09-30 05:02:45 +0530

    read

---
+++ Notebooks/read_chat_DM.py
@@ -0,0 +1,102 @@
+'''
+
+This is used to convert messages to dataframe in DMs
+
+Message format : 
+    9/7/14, 10:57 PM - BD: Yo yo
+    MM/DD/YY, HH:MM AM/PM - Name: Message
+    
+    There can be multi line messages also so we need to check the start of the line if it is date then we are in fresh message
+'''
+import re
+def hasDate(l,os=0):
+    # android 
+    pattern = '^([0-9]+)(\/)([0-9]+)(\/)([0-9]+), ([0-9]+):([0-9]+)[ ]?(AM|PM|am|pm)? -'  # RE for  MM/DD/YY, HH:MM AM/PM -
+    # IOS
+    if(os==1):
+        pattern = '^([0-9]+)(\/)([0-9]+)(\/)([0-9]+), ([0-9]+):([0-9]+)[ ]?(AM|PM|am|pm)? -' 
+    result = re.match(pattern, l)
+    if result:
+        return True
+    else:
+        return False
+
+'''
+There are six possiblites for Name
+    - First Name
+    - First Name + Last Name
+    - First Name + Middle Name + Last Name
+    - Mobile Number (India)
+    - Mobile Number (US)
+    - Mobile Number (Europe)
+'''
+def hasName(l):
+    patterns = [
+        '([\w]+):',                       
+        '([\w]+[\s]+[\w]+):',              
+        '([\w]+[\s]+[\w]+[\s]+[\w]+):',    
+        '([+]\d{2} \d{5} \d{5}):',         
+        '([+]\d{2} \d{3} \d{3} \d{4}):',   
+        '([+]\d{2} \d{4} \d{7})'          
+    ]
+    pattern = '^' + '|'.join(patterns)
+    result = re.match(pattern, l)
+    if result:
+        return True
+    else:
+        return False
+
+'''
+Function to get a message in structured form
+
+    MM/DD/YY, HH:MM AM/PM - Name: Message
+'''
+def getStructured(l):    
+    split_line = l.split(' - ') 
+    time_stamp = split_line[0]              # dateTime = MM/DD/YY, HH:MM AM/PM
+    date , time = time_stamp.split(', ')    # date = MM/DD/YY  time = HH:MM AM/PM
+    msg = ' '.join(split_line[1:])          # message = Name: Message
+    txt,name = None,None
+    if hasName(msg):
+        split_message = msg.split(': ') 
+        name = split_message[0]
+        txt = ' '.join(split_message[1:]) 
+    else:
+        name = None
+    if(txt == None):
+        return date, time, name,''
+    else:
+        return date, time, name,txt
+
+'''
+Process entire file
+'''
+
+import pandas as pd
+def getMessages(path):
+    res = [] 
+    with open(path, encoding="utf-8") as fp:
+        #fp.readline() 
+        #since message can be in multiple lines we will use buffer to store consecutive lines
+        buffer = [] 
+        date, time, author = None, None, None
+        while True:
+            line = fp.readline() 
+            if not line: 
+                break
+            line = line.strip() # clean ;)
+            # if a message starts with date then it's a new message 
+            # else it's a conti of previous message 
+            if hasDate(line):                                           # new message
+                if len(buffer) > 0:                                     # write prev message to buffer
+                    res.append([date, time, author, ' '.join(buffer)]) 
+                buffer.clear()                                          # Clear buffer since we are encountering a new message
+                date, time, author, message = getStructured(line)       # Get structured format and store it 
+                buffer.append(message)                                  # Append message to buffer
+            else:
+                res.append([date, time, author, ' '.join(buffer)])                                     # Same message in next line 
+    df = pd.DataFrame(res, columns=['Date', 'Time', 'Author', 'Message'])
+    return df
+
+# a = getMessages("Notebooks/data/chat.txt")
+# print(a.head())
